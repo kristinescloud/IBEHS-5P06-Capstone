77379274b6859cab735cf9fa526a4fabe660e1cb
PI <dami.fadiya@gmail.com>
2024-02-12 23:42:18 -0500

    moving test videos

--- dual_cameras_pic.py
+++ dual_cameras_pic.py
@@ -11,7 +11,7 @@ video_config1= cam1.create_video_configuration()
 cam1.configure(video_config1)
 
 encoder1 = H264Encoder(10000000)
-output1 = FfmpegOutput('testcam1diffdirections.mp4')
+output1 = FfmpegOutput('testcam1movingpeople.mp4')
 
 cam2 = Picamera2(1)
 cam2.start_preview(Preview.QTGL, x=500,y=300,width=400,height=300)
@@ -20,7 +20,7 @@ video_config2 = cam2.create_video_configuration()
 cam2.configure(video_config2)
 
 encoder2= H264Encoder(10000000)
-output2 = FfmpegOutput('testcam2diffdirections.mp4')
+output2 = FfmpegOutput('testcam2movingpeople.mp4')
 
 cam1.start_recording(encoder1, output1)
 cam2.start_recording(encoder2, output2)
