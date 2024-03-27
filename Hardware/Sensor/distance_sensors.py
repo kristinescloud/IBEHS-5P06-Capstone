712cf96f5acdca460190f714cd16cffd0a131219
kristinescloud <kristine.uchendu@gmail.com>
2024-03-26 20:51:25 -0400

    edit sensor with better value and delete video

--- Hardware/Sensor/distance_sensors.py
+++ Hardware/Sensor/distance_sensors.py
@@ -3,16 +3,22 @@ from time import sleep
 
 
 sensor1=DistanceSensor(echo=24,trigger=23,max_distance=2)
-sensor2=DistanceSensor(echo=17,trigger=27,max_distance=2)
-motor = PWMOutputDevice(22)
+sensor2=DistanceSensor(echo=27,trigger=17,max_distance=2)
+motor = PWMOutputDevice(16)
+motor2 = PWMOutputDevice(12)
 
-history1 = [0,0,0,0,0]
-history2 = [0,0,0,0,0]
-delta1 = [0,0,0,0]
-delta2 = [0,0,0,0]
+history1 = [0]*10
+history2 = [0]*10
+delta1 = [0]*9
+delta2 = [0]*9
+counter1 = 0
+counter2 = 0
+
+#TODO: pulsing vibration
 
 def calculate_vibration(distance):
-    vibration = (-(distance-2)/198) + 1
+    vibration = 1
+    #vibration = (-(distance-2)/198) + 1
     return vibration
 
 while True:
@@ -34,20 +40,44 @@ while True:
         delta1[i] = delta1[i+1]
         delta2[i] = delta2[i+1]
 
-    delta1 = history1[-1] - history1[-2]
-    delta2 = history2[-1] - history2[-2]
+    delta1_new = history1[-1] - history1[-2]
+    delta2_new = history2[-1] - history2[-2]
 
-    if distance1 < 20:
-
-        if delta1 > 10:
-            print("STOP Left")
-            vibration1 = calculate_vibration(distance1)
-            print("vibration1", vibration1)
-            motor.value = vibration1
+    if distance1 < 20: #20cm
+        counter1 += 1
+        
+        print("counter1:", counter1)
+        print("delta1:", delta1_new)
+        
+        print("STOP Left")
+        vibration1 = calculate_vibration(distance1)
+        print("vibration1", vibration1)
+        motor.value = vibration1
+        
+        if counter1 > 50:
+            if delta1_new > -10:
+                motor.value = 0
+    else:
+        counter1 = 0
+        motor.value = 0
         
     if distance2 < 20:
-        if delta2 > 10:
-            print("STOP Right")
+        counter2 += 1
+        
+        print("counter2:", counter2)
+        print("delta2:", delta2_new)
+        
+        print("STOP Right")
+        vibration2 = calculate_vibration(distance2)
+        print("vibration2", vibration2)
+        motor2.value = vibration2
+        
+        if counter2 > 50:
+            if delta2_new > -10:
+                motor2.value = 0
+    else:
+        counter2 = 0
+        motor2.value = 0
+    
 
-    sleep(0.5)
-   
+    sleep(0.1)
